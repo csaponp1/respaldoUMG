51bf2113923150c528b90f68a1c3c934f48632b9
unknown <csaponp1@miumg.edu.gt>
2022-08-07 19:18:14 -0600

    Primer_commit

---
+++ src/calculadora/Operaciones.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package calculadora;
+
+/**
+ *
+ * @author Sapón Pérez
+ */
+public class Operaciones {
+    
+    // agregar opcion de escribir
+    public double result;
+    
+    public void suma(double a, double b){
+        result = a+b;
+    }
+    
+    public void resta(double a, double b){
+        result = a-b;
+    }
+    
+    public void mult(double a, double b){
+        result = a*b;
+    }
+        
+    public void division(double a, double b){
+        result = a/b;
+    }    
+}
